efed2fafbd28cb0a3bf3f65a564ee29123bdd0ea
finlaywashere <finman292004@protonmail.com>
2021-09-17 21:09:20 -0400

    Modified inventory system to hook into internal services system for authentication (will be published soon)

--- api/private/db.php
+++ api/private/db.php
@@ -1,18 +1,5 @@
 <?php
 
-require_once "config.php";
-
-function db_connect(){
-	GLOBAL $dbhost;
-	GLOBAL $dbuser;
-	GLOBAL $dbpass;
-	GLOBAL $dbname;
-
-	$conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
-	if($conn->connect_error){
-		return 0;
-	}
-	return $conn;
-}
+require_once "../../../private/db.php";
 
 ?>
